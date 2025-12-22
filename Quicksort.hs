a9250ba62407543e8fea3e73cbe45f4789999a1a
Aiden Cullo <aidencullo@gmail.com>
2025-12-22 08:13:25 -0500

    quicksort working w tests

--- Quicksort.hs
+++ Quicksort.hs
@@ -1,6 +1,9 @@
-quickSort :: [a] -> [a]
--- quickSort (x:xs) =
---   let left = filter xs (<x)
+quickSort :: Ord a => [a] -> [a]
+quickSort (x:xs) =
+  let left = filter (<=x) xs
+      right = filter (>x) xs
+  in quickSort(left) ++ [x] ++ quickSort(right)
+quickSort [] = []
 -- f x =
 --   let y = x + 1
 --       z = y * 2
